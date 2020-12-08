c4e5a0d2e000cf5cf7d7e2af7971d2c65493516c
Your Name <thomassimon@Thomass-MacBook-Pro.local>
2020-12-08 13:24:58 -0600

    delete

--- src/components/header.js
+++
@@ -1,2 +0,0 @@
-import React, { Component } from "react";
-export default class Header extends Component {render(){return (<div className="header"><h1>Welcome to the employee directory</h1><p>Filter using name</p></div>)}}
